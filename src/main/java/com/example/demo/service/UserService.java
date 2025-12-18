a758eaeff05d41cb32cc864ad248d62cc481625b
bala-amypo <bala@amypo.in>
2025-12-18 09:34:31 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,54 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.User;
+
+import com.example.demo.exception.*;
+
+import com.example.demo.repository.UserRepository;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.*;
+
+public class UserService {
+
+    private final UserRepository repo;
+
+    private final PasswordEncoder encoder;
+
+    public UserService(UserRepository repo, PasswordEncoder encoder) {
+
+        this.repo = repo;
+
+        this.encoder = encoder;
+
+    }
+
+    public User registerUser(User user) {
+
+        if (repo.findByEmail(user.getEmail()).isPresent())
+
+        throw new ValidationException("Email already in use");
+
+        user.setPassword(encoder.encode(user.getPassword()));
+
+        return repo.save(user);
+
+    }
+
+    public User getUser(Long id) {
+
+        return repo.findById(id).orElseThrow(() -> new ResourceNotFoundException("User not found"));
+
+    }
+
+    public List<User> getAllUsers() { return repo.findAll(); }
+
+    public User getByEmail(String email) {
+
+        return repo.findByEmail(email).orElseThrow(() -> new ResourceNotFoundException("User not found"));
+
+    }
+
+}
+
