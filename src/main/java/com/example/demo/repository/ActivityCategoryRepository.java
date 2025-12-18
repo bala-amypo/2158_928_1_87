d6c0b52cfeeff65591bbd0662b804a5ca0d86de6
bala-amypo <bala@amypo.in>
2025-12-18 09:23:36 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/ActivityCategoryRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.ActivityCategory;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ActivityCategoryRepository extends JpaRepository<ActivityCategory, Long> {
+
+    boolean existsByCategoryName(String name);
+
+}
