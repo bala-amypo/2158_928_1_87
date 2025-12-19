171c7489da385dda524d2d41aaa12926b057c9aa
bala-amypo <bala@amypo.in>
2025-12-19 04:27:30 +0000

    User commit

---
+++ src/main/java/com/example/demo/security/JwtUtil.java
@@ -0,0 +1,72 @@
+package com.example.demo.security;
+
+import io.jsonwebtoken.*;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+
+public class JwtUtil {
+
+    private final String SECRET = "secret-key";
+
+    private final long EXPIRATION = 1000 * 60 * 60; // 1 hour
+
+    public String generateToken(String email) {
+
+        return Jwts.builder()
+
+        .setSubject(email)
+
+        .setIssuedAt(new Date())
+
+        .setExpiration(
+
+            new Date(System.currentTimeMillis() + EXPIRATION))
+
+            .signWith(SignatureAlgorithm.HS256, SECRET)
+
+            .compact();
+
+    }
+
+    public String extractEmail(String token) {
+
+        return Jwts.parser()
+
+        .setSigningKey(SECRET)
+
+        .parseClaimsJws(token)
+
+        .getBody()
+
+        .getSubject();
+
+    }
+
+    public boolean validateToken(String token) {
+
+        try {
+
+            Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token);
+
+            return true;
+
+        } catch (Exception e) {
+
+            return false;
+
+        }
+
+    }
+
+}
+        }
+        }
+    }
+    }
+        )
+    }
+}
