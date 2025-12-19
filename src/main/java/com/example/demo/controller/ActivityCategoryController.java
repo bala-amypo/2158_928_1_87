0fefcfc27ba9b528412ea7218324bdd557150ff3
bala-amypo <bala@amypo.in>
2025-12-19 03:57:30 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/ActivityCategoryController.java
@@ -0,0 +1,50 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.ActivityCategory;
+
+import com.example.demo.service.ActivityCategoryService;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+
+@RequestMapping("/categories")
+
+public class ActivityCategoryController {
+
+    private final ActivityCategoryService service;
+
+    public ActivityCategoryController(ActivityCategoryService service) {
+
+        this.service = service;
+
+    }
+
+    @PostMapping
+
+    public ActivityCategory create(@RequestBody ActivityCategory category) {
+
+        return service.createCategory(category);
+
+    }
+
+    @GetMapping("/{id}")
+
+    public ActivityCategory get(@PathVariable Long id) {
+
+        return service.getCategory(id);
+
+    }
+
+    @GetMapping
+
+    public List<ActivityCategory> getAll() {
+
+        return service.getAllCategories();
+
+    }
+
+}
+  
