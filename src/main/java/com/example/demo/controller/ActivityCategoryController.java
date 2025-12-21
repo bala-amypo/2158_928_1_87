8397eb26233bc864200aa74f7fc37ac3c044d44f
bala-amypo <bala@amypo.in>
2025-12-21 05:46:54 +0000

    User commit

--- src/main/java/com/example/demo/controller/ActivityCategoryController.java
+++ src/main/java/com/example/demo/controller/ActivityCategoryController.java
@@ -8,43 +8,63 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+// @RestController
 
+// @RequestMapping("/categories")
+
+// public class ActivityCategoryController {
+
+//     private final ActivityCategoryService service;
+
+//     public ActivityCategoryController(ActivityCategoryService service) {
+
+//         this.service = service;
+
+//     }
+
+//     @PostMapping
+
+//     public ActivityCategory create(@RequestBody ActivityCategory category) {
+
+//         return service.createCategory(category);
+
+//     }
+
+//     @GetMapping("/{id}")
+
+//     public ActivityCategory get(@PathVariable Long id) {
+
+//         return service.getCategory(id);
+
+//     }
+
+//     @GetMapping
+
+//     public List<ActivityCategory> getAll() {
+
+//         return service.getAllCategories();
+
+//     }
+
+// }
+ @RestController
 @RequestMapping("/categories")
-
 public class ActivityCategoryController {
 
     private final ActivityCategoryService service;
 
     public ActivityCategoryController(ActivityCategoryService service) {
-
         this.service = service;
-
     }
 
     @PostMapping
-
-    public ActivityCategory create(@RequestBody ActivityCategory category) {
-
-        return service.createCategory(category);
-
-    }
-
-    @GetMapping("/{id}")
-
-    public ActivityCategory get(@PathVariable Long id) {
-
-        return service.getCategory(id);
-
+    public ActivityCategory create(@Valid @RequestBody ActivityCategory c) {
+        return service.create(c);
     }
 
     @GetMapping
-
     public List<ActivityCategory> getAll() {
-
-        return service.getAllCategories();
-
+        return service.getAll();
     }
-
 }
-  
+ 
