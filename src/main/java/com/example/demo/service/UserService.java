49c5470f0978928352ead0a7ad2698117d4dd8a0
bala-amypo <bala@amypo.in>
2025-12-21 16:31:13 +0000

    User commit

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,12 +1,9 @@
 package com.example.demo.service;
 
-import java.util.List;
-
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
-import com.example.demo.exception.ValidationException;
 
 @Service
 public class UserService {
@@ -17,24 +14,11 @@ public class UserService {
         this.repo = repo;
     }
 
-    // Used by login
     public User getByEmail(String email) {
         return repo.findByEmail(email).orElse(null);
     }
 
-    // Used by register
     public User save(User user) {
         return repo.save(user);
-    }
-
-    //  Used by UserController
-    public List<User> getAllUsers() {
-        return repo.findAll();
-    }
-
-    //  Used by UserController
-    public User getUser(Long id) {
-        return repo.findById(id)
-                .orElseThrow(() -> new ValidationException("User not found"));
     }
 }
