b03fb6b9eebbfd01a2ba0f53166fa61dbdb24d64
bala-amypo <bala@amypo.in>
2025-12-21 06:31:54 +0000

    User commit

--- src/main/java/com/example/demo/service/ActivityTypeService.java
+++ src/main/java/com/example/demo/service/ActivityTypeService.java
@@ -6,6 +6,8 @@ import com.example.demo.repository.ActivityCategoryRepository;
 import com.example.demo.repository.ActivityTypeRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ActivityTypeService {
 
@@ -26,69 +28,8 @@ public class ActivityTypeService {
         type.setCategory(category);
         return typeRepo.save(type);
     }
+
+    public List<ActivityType> getAll() {
+        return typeRepo.findAll();
+    }
 }
-
-
-// package com.example.demo.service;
-
-
-
-// import com.example.demo.entity.*;
-
-// import com.example.demo.exception.*;
-
-// import com.example.demo.repository.*;
-
-// import java.util.*;
-
-
-// import org.springframework.stereotype.Service;
-
-// @Service
-// public class ActivityTypeService {
-
-//     private final ActivityTypeRepository typeRepo;
-
-//     private final ActivityCategoryRepository catRepo;
-
-
-
-//     public ActivityTypeService(ActivityTypeRepository t, ActivityCategoryRepository c) {
-
-//         this.typeRepo = t;
-
-//         this.catRepo = c;
-
-//         }
-
-
-
-//         public ActivityType createType(Long categoryId, ActivityType type) {
-
-//             ActivityCategory cat = catRepo.findById(categoryId)
-
-//             .orElseThrow(() -> new ResourceNotFoundException("Category not found"));
-
-//             type.setCategory(cat);
-
-//             return typeRepo.save(type);
-
-//         }
-
-
-
-//         public ActivityType getType(Long id) {
-
-//             return typeRepo.findById(id).orElseThrow(() -> new ResourceNotFoundException("Category not found"));
-
-//         }
-
-
-
-//         public List<ActivityType> getTypesByCategory(Long id) {
-
-//             return typeRepo.findByCategory_Id(id);
-
-//         }
-
-// }
